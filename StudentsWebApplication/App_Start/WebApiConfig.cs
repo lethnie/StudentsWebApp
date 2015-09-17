95481bb670f8b19ae4cd9b48e8a7b3de078cfa78
lethnie <lethnie@gmail.com>
2015-09-17 13:52:31 +0300

    students controller + index.html

--- StudentsWebApplication/App_Start/WebApiConfig.cs
+++ StudentsWebApplication/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace StudentsWebApplication
 {
@@ -10,7 +11,7 @@ namespace StudentsWebApplication
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
---
+++ StudentsWebApplication/Controllers/StudentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentsWebApplication.Models;
+using Newtonsoft.Json;
+
+namespace StudentsWebApplication.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        StudentsDbEntities dbContext = new StudentsDbEntities();
+
+        public String GetAllStudents()
+        {
+            var students = dbContext.Students.Select(s => new { s.Name, s.Age });
+            return JsonConvert.SerializeObject(students);
+        }
+
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = dbContext.Students.Where(s => s.Id == id).Select(s => new { s.Name, s.Age }).FirstOrDefault();
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(JsonConvert.SerializeObject(student));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
